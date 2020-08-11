33a500059897c0b4dfdc7efcb128bd62394b86f4
tannerb9 <tanner-brainard@hotmail.com>
2020-08-11 14:01:24 -0500

    feat: created an employee detail view

--- hrapp/models/employee_computer.py
+++ hrapp/models/employee_computer.py
@@ -2,6 +2,7 @@ from django.db import models
 from .employee import Employee
 from .computer import Computer
 
+
 class EmployeeComputer(models.Model):
     """
     Creates the join table for the many to many relationship between computers and employees
@@ -9,17 +10,17 @@ class EmployeeComputer(models.Model):
     methods: none
     """
 
-    employee = models.ForeignKey(Employee, 
-        related_name="EmployeeComputers",  
-        null=True, # Makes column nullable in DB
-        blank=True, # Allows blank value on objects
-        on_delete=models.CASCADE)
-    computer = models.ForeignKey(Computer,  
-        related_name="EmployeesComputer",  
-        null=True, # Makes column nullable in DB
-        blank=True, # Allows blank value on objects
-        on_delete=models.CASCADE)
-    assigned_date = models.DateField(auto_now=False, auto_now_add=False)
-    unassigned_date = models.DateField(auto_now=False, auto_now_add=False)
-    
-
+    employee = models.ForeignKey(Employee,
+                                 related_name="EmployeeComputers",
+                                 null=True,  # Makes column nullable in DB
+                                 blank=True,  # Allows blank value on objects
+                                 on_delete=models.CASCADE)
+    computer = models.ForeignKey(Computer,
+                                 related_name="EmployeesComputer",
+                                 null=True,  # Makes column nullable in DB
+                                 blank=True,  # Allows blank value on objects
+                                 on_delete=models.CASCADE)
+    assigned_date = models.DateField(
+        null=True, auto_now=False, auto_now_add=False)
+    unassigned_date = models.DateField(
+        null=True, auto_now=False, auto_now_add=False)
