7914e5f840ad05b30d8a0ddcf1d80efe014d85a6
Adrian Garmendia <dejima@gmail.com>
2020-08-13 13:23:50 -0500

    wip

---
+++ hrapp/views/computers/computer_details.py
@@ -0,0 +1,32 @@
+import sqlite3
+from django.shortcuts import render, reverse, redirect
+from hrapp.models import Computer
+from ..connection import Connection
+
+
+def get_computer(computer_id):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = sqlite3.Row
+
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        SELECT
+            c.id,
+                c.make,
+                c.purchase_date,
+                c.decommission_date,
+                c.manufacturer
+            from hrapp_computer c
+        """, (comouter_id,))
+
+        return db_cursor.fetchone()
+
+
+def computer_details(request, computer_id):
+    if request.method == "GET":
+        computer = get_computer(computer_id)
+        template = "computers/computer_details.html"
+
+        return render(request, template, {"computer": computer})
+
+    
