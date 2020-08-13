c8f3f903b79dfd6ca185cd72105ce37324acd374
tannerb9 <tanner-brainard@hotmail.com>
2020-08-13 15:32:10 -0500

    chore: commit before checking PR

--- hrapp/views/departments/department_details.py
+++ hrapp/views/departments/department_details.py
@@ -1,20 +1,43 @@
 import sqlite3
 from django.shortcuts import render, reverse, redirect
-from hrapp.models import Department
+from hrapp.models import Department, Employee
 from ..connection import Connection
+
+
+def create_department(cursor, row):
+    _row = sqlite3.Row(cursor, row)
+
+    department = Department()
+    department.id = _row["department_id"]
+    department.department_name = _row["department_name"]
+    department.department_budget = _row["department_budget"]
+
+    department.employees = []
+
+    employee = Employee()
+    employee.id = _row["employee_id"]
+    employee.first_name = _row["first_name"]
+    employee.last_name = _row["last_name"]
+
+    return department
 
 
 def get_department(department_id):
     with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = sqlite3.Row
+        conn.row_factory = create_department
 
         db_cursor = conn.cursor()
         db_cursor.execute("""
-        select
+        SELECT
                 d.id,
                 d.department_name,
-                d.department_budget
-            from hrapp_department d
+                d.department_budget,
+                e.id employee_id,
+                e.department_id,
+                e.first_name,
+                e.last_name
+            FROM hrapp_department d
+            JOIN hrapp_employee e ON e.department_id = d.id
         WHERE d.id = ?
         """, (department_id,))
 
