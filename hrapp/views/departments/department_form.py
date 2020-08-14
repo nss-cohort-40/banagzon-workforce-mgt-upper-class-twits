0800e7e1ea6f98f181355e2f88f518fc65b06838
tannerb9 <tanner-brainard@hotmail.com>
2020-08-14 01:36:48 -0500

    chore: removed zombie code

--- hrapp/views/departments/department_form.py
+++ hrapp/views/departments/department_form.py
@@ -1,13 +1,12 @@
 import sqlite3
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from ..connection import Connection
 from hrapp.models import Department
 
-# @login_required
+
 def department_form(request):
     if request.method == 'GET':
-       
+
         template = 'departments/department_form.html'
 
         return render(request, template)
